e314e262257b51d5b3b3eaff116ad90e53af16d5
Hendrix Tavarez <htavarez@gmail.com>
2024-11-26 19:06:07 -0500

    nov 26

--- project2/Grid.java
+++ project2/Grid.java
@@ -1,3 +1,10 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 class Grid {
 
   private int[][] grid;
@@ -38,6 +45,20 @@ class Grid {
     }
   }
 
+  /**
+   * This method creates a two-dimensional array called countGrid which will
+   * store the number of 1s in the current cell and its neighbours
+   *
+   * if the grid two-dimensional array is as follows:
+   * [ 0] [0 ] [0 ]
+   * [ 1] [1 ] [0 ]
+   * [ 0] [0 ] [1 ]
+   * 
+   * then the countGrid two-dimensional array should be as follows:
+   * [ 2] [2 ] [1 ]
+   * [ 2] [3 ] [2 ]
+   * [ 2] [3 ] [2 ]
+   */
   private void createCountGrid() {
     int rows = 3;
     int cols = 3;
@@ -61,11 +82,15 @@ class Grid {
     }
   }
 
+  /**
+   * This method returns the number of neighbours of the current cell
+   * 
+   * @param row
+   * @param col
+   * @return the number of neighbours of the current cell
+   */
   private int getNeighbours(int row, int col) {
     int neighbours = 0;
-    // [ 0] [0 ] [0 ]
-    // [ 1] [1 ] [0 ]
-    // [ 0] [0 ] [1 ]
 
     // let's check the cell directly above the current cell
     if (row - 1 >= 0) {
@@ -126,6 +151,28 @@ class Grid {
     return neighbours;
   }
 
+  public class ButtonActionListener implements ActionListener {
+    private int row;
+    private int col;
+    private int[][] grid;
+
+    public ButtonActionListener(int[][] grid, int row, int col) {
+      this.grid = grid;
+      this.row = row;
+      this.col = col;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      JButton button = (JButton) e.getSource();
+      if (grid[row][col] == 1) {
+        button.setText("Bomb");
+      } else {
+        button.setText("No");
+      }
+    }
+  }
+
   public static void main(String[] args) {
 
     Grid g = new Grid();
@@ -148,6 +195,32 @@ class Grid {
       }
       System.out.println();
     }
+
+    JFrame frame = new JFrame();
+    JPanel board = new JPanel();
+    board.setLayout(new GridLayout(3, 3));
+    for (int i = 0; i < g.grid.length; i++) { // rows
+      for (int j = 0; j < g.grid[i].length; j++) { // columns
+        JButton button = new JButton();
+
+        button.setFont(button.getFont().deriveFont(24.0f));
+        // button.setText(g.countGrid[i][j] + "");
+        button.addActionListener(g.new ButtonActionListener(g.grid, i, j));
+        board.add(button);
+
+      }
+    }
+    frame.add(board);
+
+    // at this point, there are 3 2-dimensional arrays.
+    // The first one is the grid with 1s and 0s
+    // The second one is the countGrid with the number of 1s in the current cell and
+    // its neighbours
+    // The third one is the board which is a JPanel with 9 JButtons
+
+    frame.setSize(300, 300);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
 
   }
 }
