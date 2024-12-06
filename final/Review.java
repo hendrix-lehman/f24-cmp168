e6f9e162f09e0619c86de8d466953200a8339685
Hendrix Tavarez <htavarez@gmail.com>
2024-12-05 19:47:15 -0500

    dev 5

---
+++ final/Review.java
@@ -0,0 +1,15 @@
+class Review {
+
+  public static void main(String[] args) {
+
+    final int SIZE = 5;
+    int[] x = new int[SIZE];
+
+    x[0] = 10;
+
+    int i;
+    for (i = 0; i <= SIZE; ++i) {
+      System.out.println(x[i]);
+    }
+  }
+}
